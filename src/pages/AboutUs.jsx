016b913af8f98abdd38439be8a1bbe2afda9adc6
Michael Flores <mich4el.flores@gmail.com>
2022-05-23 14:20:36 -0300

    Hotfixs

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -4,10 +4,10 @@ import Layout from "../core/Layout";
 export default function AboutUs() {
   return (
     <Layout>
-      <div className="w-full min-h-screen flex items-center justify-around">
-        <img src="/logo.png" alt="Logo"/>
-        <h1 className="text-center" style={{ fontSize: "100px" }}>
-          ANIMA WARRIORS
+      <div className="w-full flex items-center justify-around">
+        <img src="/logo.png" alt="Logo" className="pl-20" />
+        <h1 className="pl-20" style={{ fontSize: "100px" }}>
+          ANIMA <span className="font-bold">WARRIORS</span>
         </h1>
       </div>
     </Layout>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <Layout>
       <div className="w-full flex items-center justify-around">
-        <img src="/logo.png" alt="Logo" />
-        <h1 className="text-center" style={{ fontSize: "100px" }}>
-          ANIMA WARRIORS
+        <img src="/logo.png" alt="Logo" className="pl-20"/>
+        <h1 className="pl-20" style={{ fontSize: "100px" }}>
+          ANIMA <span className="font-bold">WARRIORS</span>
         </h1>
       </div>
     </Layout>
